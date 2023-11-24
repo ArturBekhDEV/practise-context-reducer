5bd8ea72c635d20cc052e07065d7d7b91b2ee2b0
Artur Bekh <artur@Arturs-MacBook-Pro.local>
2023-11-24 11:01:22 +0200

    186 lesson

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
 import Product from "../src/components/Product.jsx";
+import { CartContext } from "./store/shop-context.jsx";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -68,17 +69,19 @@ function App() {
 
   return (
     <>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
-      <Shop onAddItemToCart={handleAddItemToCart}>
-        {DUMMY_PRODUCTS.map((product) => (
-          <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
-          </li>
-        ))}
-      </Shop>
+      <CartContext.Provider>
+        <Header
+          cart={shoppingCart}
+          onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
+        />
+        <Shop onAddItemToCart={handleAddItemToCart}>
+          {DUMMY_PRODUCTS.map((product) => (
+            <li key={product.id}>
+              <Product {...product} onAddToCart={handleAddItemToCart} />
+            </li>
+          ))}
+        </Shop>
+      </CartContext.Provider>
     </>
   );
 }
